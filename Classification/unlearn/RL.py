d173a2af78c03967394770714d309d1ea29c6527
filiperobotic <filipe.rolim@gmail.com>
2026-02-04 19:15:37 -0300

    wqqwaa

--- Classification/unlearn/RL.py
+++ Classification/unlearn/RL.py
@@ -15,9 +15,9 @@ def RL(data_loaders, model, criterion, optimizer, epoch, args, mask=None):
     forget_dataset = deepcopy(forget_loader.dataset)
     
     #if args.dataset == "cifar100" or args.dataset == "TinyImagenet":
-    if args.dataset == "cifar100" or args.dataset == "TinyImagenet" or args.dataset in ["bloodmnist", "pathmnist", "organamnist", "octmnist"]:
+    if args.dataset == "cifar100" or args.dataset == "TinyImagenet" or args.dataset in ["bloodmnist", "pathmnist", "organamnist", "octmnist", "dermamnist_bin"]:
         
-        if args.dataset in ["bloodmnist", "pathmnist", "organamnist", "octmnist"]:
+        if args.dataset in ["bloodmnist", "pathmnist", "organamnist", "octmnist", "dermamnist_bin"]:
             forget_dataset.targets = np.random.randint(0, args.num_classes, forget_dataset.labels.shape)
         else:
         
